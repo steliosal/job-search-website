4bc72b1104402b8ee896f0f067be049a5256e29f
HOME-PC\Stelios <steliosdirects@gmail.com>
2022-08-01 09:43:40 +0300

    install tailwind and open sans fonts

---
+++ src/playground.js
@@ -0,0 +1,6 @@
+const fruits = ["Apple", "Banana", "Orange"];
+const vegetables = ["Cucumber", "Radish"];
+const newArray = fruits.concat(vegetables);
+const newArray2 = [...fruits, ...vegetables];
+console.log(newArray);
+console.log(newArray2);

a380aae38e23177339ca6a0c3657fe678f2f38e0
HOME-PC\Stelios <steliosdirects@gmail.com>
2022-08-04 12:45:11 +0300

    Main Nav Component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Yo YOoooooooooooooo</h1>
+  <MainNav class="text-center text-2xl bg-slate-900 text-white" />
+  <h1 class="mt-8 text-6xl text-center">Hello Vue</h1>
 </template>
 
 <script>
+import MainNav from "@/components/MainNav.vue";
+
 export default {
   name: "App",
+  components: {
+    MainNav,
+  },
 };
 </script>

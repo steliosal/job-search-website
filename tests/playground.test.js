75d54935e2a680dacb6195cd900e93c3b4b766ea
HOME-PC\Stelios <steliosdirects@gmail.com>
2022-08-05 12:46:42 +0300

    jesting

--- tests/playground.test.js
+++ tests/playground.test.js
@@ -1,3 +1,5 @@
+//npm run test:unit
+//
 import { evenOrOdd } from "@/playground";
 
 describe("basic Math", () => {
@@ -9,5 +11,17 @@ describe("basic Math", () => {
     expect(5 - 3).toBe(2);
   });
 
-  describe("evenOrOdd", () => {});
+  describe("evenOrOdd", () => {
+    describe("when the number is even", () => {
+      it("indicates the number is even", () => {
+        expect(evenOrOdd(4)).toBe("Even");
+      });
+    });
+
+    describe("when the number is odd", () => {
+      it("indicates the number is odd", () => {
+        expect(evenOrOdd(3)).toBe("odd");
+      });
+    });
+  });
 });

00cef5c1482a8e4d10a8b492f7c4e7b035298647
HOME-PC\Stelios <steliosdirects@gmail.com>
2022-08-08 10:25:50 +0300

    navbar testing - data-test / vbind - vfor

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -6,4 +6,20 @@ describe("MainNav", () => {
     const wrapper = mount(MainNav);
     expect(wrapper.text()).toMatch("Bobo Careers");
   });
+
+  it("displays menu items for navigation", () => {
+    const wrapper = mount(MainNav);
+    const navigationMenuItems = wrapper.findAll(
+      "[data-test='main-nav-list-time']"
+    );
+    const navigationMenuTexts = navigationMenuItems.map((item) => item.text());
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Bobo",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
+  });
 });

58ef773b7676eeb3e5016f5b2db22c297d09e372
HOME-PC\Stelios <steliosdirects@gmail.com>
2022-08-09 20:05:39 +0300

    passing props & creating secondary button style

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,22 @@
 <template>
-  <button
-    class="px-5 py-2 font-medium text-white border-0 rounded bg-brand-blue-1 hover:shadow-blue"
-  >
-    Sign In
+  <button class="primary">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text"],
 };
 </script>
+
+<style scoped>
+button {
+  @apply px-5 py-2 font-medium rounded;
+}
+
+.primary {
+  @apply text-white bg-brand-blue-1 hover:shadow-blue;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -23,7 +23,12 @@
         </nav>
         <div class="flex items-center h-full ml-auto">
           <ProfileImage v-if="isLoggedIn" data-test="profile-image" />
-          <ActionButton v-else data-test="login-button" @click="loginUser" />
+          <ActionButton
+            v-else
+            text="Sign In"
+            data-test="login-button"
+            @click="loginUser"
+          />
         </div>
       </div>
     </div>
